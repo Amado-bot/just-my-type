bc3020cb81a72458f4663308a99785be0baab592
unknown <amadocardenas0@gmail.com>
2021-05-03 17:42:00 -0600

    update css

--- public/javascript/typing.js
+++ public/javascript/typing.js
@@ -145,6 +145,32 @@ function resetValues() {
     wpm_group.style.display = "none";
 }
 
+function finishGame() {
+    // stop the timer
+    clearInterval(timer);
+    
+    // disable the input area
+    input_area.disabled = true;
+    
+    // show finishing text
+    quote_text.textContent = "Click on restart to start a new game.";
+    
+    // display restart button
+    restart_btn.style.display = "block";
+    
+    // calculate cpm and wpm
+    cpm = Math.round(((characterTyped / timeElapsed) * 60));
+    wpm = Math.round((((characterTyped / 5) / timeElapsed) * 60));
+    
+    // update cpm and wpm text
+    cpm_text.textContent = cpm;
+    wpm_text.textContent = wpm;
+    
+    // display the cpm and wpm
+    cpm_group.style.display = "block";
+    wpm_group.style.display = "block";
+  }
+
 // document.getElementById("restartButton").addEventListener("click", function () {
 //     document.getElementById('textInput').value = '';
 //     location.reload();
