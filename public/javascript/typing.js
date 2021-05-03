b51489655ebd9ad96fbba6293709b724b3e70fc9
unknown <amadocardenas0@gmail.com>
2021-05-03 17:58:43 -0600

    I forgot why i was commiting

--- public/javascript/typing.js
+++ public/javascript/typing.js
@@ -25,32 +25,129 @@ let current_quote = "";
 let quoteNo = 0;
 let timer = null;
 
-async function getRandomText() {
-    const randomText = await fetch(random_text_url);
-    return await randomText.json();
-};
+// async function getRandomQuote() {
+//     const randomText = await fetch(random_text_url);
+//     return await randomText.json();
+// };
 
-async function renderRandomText() {
-    await getRandomText()
-    quote_text.innerText = randomText
-    quote_text.innerHTML = ''
-    randotext.split('').forEach(char => {
-        const charSpan = document.createElement('span')
-        charSpan.innerText = char
-        quoteText.appendChild(charSpan)
+// async function updateQuote() {
+//     await getRandomQuote()
+//     quote_text.innerText = randomText
+//     quote_text.innerHTML = ''
+//     current_quote.split('').forEach(char => {
+//         const charSpan = document.createElement('span')
+//         charSpan.innerText = char
+//         quote_text.appendChild(charSpan)
+//       })
+//     document.getElementById('textInput').value = ('');
+// }
+
+function updateQuote() {
+    quote_text.textContent = null;
+    current_quote = quotes_array[quoteNo];
+    
+    // separate each character and make an element 
+    // out of each of them to individually style them
+    current_quote.split('').forEach(char => {
+      const charSpan = document.createElement('span')
+      charSpan.innerText = char
+      quote_text.appendChild(charSpan)
     })
-    document.getElementById('text-input').value = ('');
-}
+    
+    // roll over to the first quote
+    if (quoteNo < quotes_array.length - 1)
+      quoteNo++;
+    else
+      quoteNo = 0;
+  }
 
-function startGame() {
+function processCurrentText() {
+  
+    // get current input text and split it
+    curr_input = input_area.value;
+    curr_input_array = curr_input.split('');
+    
+    // increment total characters typed
+    characterTyped++;
+    
+    errors = 0;
+    
+    quoteSpanArray = quote_text.querySelectorAll('span');
+    quoteSpanArray.forEach((char, index) => {
+      let typedChar = curr_input_array[index]
+    
+      // character not currently typed
+      if (typedChar == null) {
+        char.classList.remove('correct_char');
+        char.classList.remove('incorrect_char');
+    
+        // correct character
+      } else if (typedChar === char.innerText) {
+        char.classList.add('correct_char');
+        char.classList.remove('incorrect_char');
+    
+        // incorrect character
+      } else {
+        char.classList.add('incorrect_char');
+        char.classList.remove('correct_char');
+    
+        // increment number of errors
+        errors++;
+      }
+    });
+    
+    // display the number of errors
+    error_text.textContent = total_errors + errors;
+    
+    // update accuracy text
+    let correctCharacters = (characterTyped - (total_errors + errors));
+    let accuracyVal = ((correctCharacters / characterTyped) * 100);
+    accuracy_text.textContent = Math.round(accuracyVal);
+    
+    // if current text is completely typed
+    // irrespective of errors
+    if (curr_input.length == current_quote.length) {
+      updateQuote();
+    
+      // update total errors
+      total_errors += errors;
+    
+      // clear the input area
+      input_area.value = "";
+    }
+  }
+  
+  function startGame() {
+    
     resetValues();
-    renderRandomText();
-
+    updateQuote();
+    
+    // clear old and start a new timer
     clearInterval(timer);
-    timer = setInterval(updateTimer,1000)
-}
-
-function updateTimer() {
+    timer = setInterval(updateTimer, 1000);
+  }
+    
+  function resetValues() {
+    timeLeft = TIME_LIMIT;
+    timeElapsed = 0;
+    errors = 0;
+    total_errors = 0;
+    accuracy = 0;
+    characterTyped = 0;
+    quoteNo = 0;
+    input_area.disabled = false;
+    
+    input_area.value = "";
+    quote_text.textContent = 'Click on the area below to start the game.';
+    accuracy_text.textContent = 100;
+    timer_text.textContent = timeLeft + 's';
+    error_text.textContent = 0;
+    restart_btn.style.display = "none";
+    cpm_group.style.display = "none";
+    wpm_group.style.display = "none";
+  }
+  
+  function updateTimer() {
     if (timeLeft > 0) {
       // decrease the current time left
       timeLeft--;
@@ -66,86 +163,8 @@ function updateTimer() {
       finishGame();
     }
   }
-
-
-function processCurrentText() {
-
-    // get current input text and split it
-    curr_input = input_area.value;
-    curr_input_array = curr_input.split('');
-
-    // increment total characters typed
-    characterTyped++;
-
-    errors = 0;
-
-    quoteSpanArray = quote_text.querySelectorAll('span');
-    quoteSpanArray.forEach((char, index) => {
-        let typedChar = curr_input_array[index]
-
-        // character not currently typed
-        if (typedChar == null) {
-            char.classList.remove('correct_char');
-            char.classList.remove('incorrect_char');
-
-            // correct character
-        } else if (typedChar === char.innerText) {
-            char.classList.add('correct_char');
-            char.classList.remove('incorrect_char');
-
-            // incorrect character
-        } else {
-            char.classList.add('incorrect_char');
-            char.classList.remove('correct_char');
-
-            // increment number of errors
-            errors++;
-        }
-    });
-
-    // display the number of errors
-    error_text.textContent = total_errors + errors;
-
-    // update accuracy text
-    let correctCharacters = (characterTyped - (total_errors + errors));
-    let accuracyVal = ((correctCharacters / characterTyped) * 100);
-    accuracy_text.textContent = Math.round(accuracyVal);
-
-    // if current text is completely typed
-    // irrespective of errors
-    if (curr_input.length == current_quote.length) {
-        updateQuote();
-
-        // update total errors
-        total_errors += errors;
-
-        // clear the input area
-        input_area.value = "";
-    }
-}
-
-
-function resetValues() {
-    timeLeft = TIME_LIMIT;
-    timeElapsed = 0;
-    errors = 0;
-    total_errors = 0;
-    accuracy = 0;
-    characterTyped = 0;
-    quoteNo = 0;
-    input_area.disabled = false;
-
-    input_area.value = "";
-    quote_text.textContent = 'Click on the area below to start the game.';
-    accuracy_text.textContent = 100;
-    timer_text.textContent = timeLeft + 's';
-    error_text.textContent = 0;
-    restart_btn.style.display = "none";
-    cpm_group.style.display = "none";
-    wpm_group.style.display = "none";
-}
-
-function finishGame() {
+  
+  function finishGame() {
     // stop the timer
     clearInterval(timer);
     
@@ -171,7 +190,4 @@ function finishGame() {
     wpm_group.style.display = "block";
   }
 
-// document.getElementById("restartButton").addEventListener("click", function () {
-//     document.getElementById('textInput').value = '';
-//     location.reload();
-// });
+  
