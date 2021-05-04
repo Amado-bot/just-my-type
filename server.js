39e792bb4ec57d6e035fd58a9028c951b6402c92
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-03 18:30:58 -0600

    added helpers.js and auth.js and began Comment.js

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
 const express = require('express');
+const path = require('path')
 const exphbs = require('express-handlebars');
-const session = require('express-session');
+const session = require('express-session')
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Secret secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening.'));
+});
