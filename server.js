496eef2bd47469379c8accbde24c775886078bb7
unknown <amadocardenas0@gmail.com>
2021-05-03 19:32:16 -0600

    basic server

--- server.js
+++ server.js
@@ -3,3 +3,21 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const sequelize = require('./config/connection');
 
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
