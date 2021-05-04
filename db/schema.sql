730df3a2ae906522d57bd194d18b7ebd3e1c22fd
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-03 18:58:45 -0600

    created comment model and some index.js stuff and things

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS just_my_type_db;
+
+CREATE DATABASE just_my_type_db;
---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {
+
+}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    comment_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            // there needs to be at least one character before someone can post a comment
+            len: [1]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+
+        // does this need to reference a "Profile" model since we want to comment on profiles?
+        // references: {
+        // model: 'profile',
+        // key: 'id',
+        //}
+    }
+},
+    {
+        sequelize,
+        underscored: true,
+        modelName: 'comment',
+        freezeTableName: true
+    }
+);
