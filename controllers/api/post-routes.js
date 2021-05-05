45e49111e894ea8aa20a9098150ac75825ecc2c7
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-04 20:16:22 -0600

    created post route

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,139 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = - require('../../utils/auth');
+
+// GET ALL POSTS
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'user_id',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            // add the comment model
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                // include the User model
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log('There was an error! ' + err);
+        });
+});
+
+// GET A POST BY ITS ID
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'user_id',
+            'created_at'
+        ],
+        // vote stuff eventually
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'There is no post with this ID.' })
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log('There was an error!' + err);
+            res.status(500).json(err);
+        })
+})
+
+// CREATE A NEW POST
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        user_id: req.session.user_id
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log('There was an error! ' + err)
+            res.status(500).json
+        })
+})
+
+// UPDATE an existing POST
+router.put('/:id', (req, res) => {
+    Post.update({
+        title: req.body.title,
+        body: req.body.body
+    },
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this ID.' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log('There was an error!' + err);
+            res.status(500).json(err);
+        })
+})
+
+// DELETE request for localhost:3001/api/posts/:id
+// DESTROY an existing POST
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(400).json({ message: 'No post found with this ID.' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log('There was an error!' + err);
+            res.status(500).json(err);
+        })
+
+})
