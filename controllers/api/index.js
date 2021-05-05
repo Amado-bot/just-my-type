8f4bf0e7bd2afad2b7a457c144c2acd64376509f
matty-bennett <bennyot10@gmail.com>
2021-05-04 21:54:55 -0600

    started score model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const commentRoutes = require('./comment-routes');
 const postRoutes = require('./post-routes');
-
 const playerRoutes = require('./player-routes')
-const postRoutes = require('./post-routes');
+
 
 router.use('/players', playerRoutes)
 
