36fac13747b9a93044d96224ac31877c3c3c2f3b
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-04 19:26:32 -0600

    post model created

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+
+
+// add upvote functionality here
+
+}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        underscored: true,
+        freezeTableName: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
