4e9aa26a0455ded5e7e6a06ba0838a949e2a5155
matty-bennett <bennyot10@gmail.com>
2021-05-05 20:27:35 -0600

    started score routes

--- controllers/api/score_routes.js
+++ controllers/api/score_routes.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 
-const { Post, Player } = require('../../models');
+const { Post, Player, Score } = require('../../models');
+
+router.get('/', (req, res) => {
+    Score.findAll({
+        attributes: [
+            'id',
+            'wpm_score',
+            'accuracy_score',
+            'player_id'
+        ],
+        
+    })
+})
