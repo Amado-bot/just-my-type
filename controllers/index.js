da419cc32a35127e6fe4e0a9337c0aaf5a3745d1
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-06 18:08:57 -0600

    user can now create posts but not edit or delete

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api')
 const gameRoutes = require('./game-routes.js')
+const dashboardRoutes = require('./dashboard-routes')
 
 router.use('/api', apiRoutes);
 router.use('/game', gameRoutes)
+router.use('/dashboard', dashboardRoutes)
 router.use('/', homeRoutes)
 
 
